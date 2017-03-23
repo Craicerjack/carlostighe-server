11e45f3e6d121cbcaa26f094402b4bab0cf2a49c
Carlos Tighe <carlos.tighe@insight-centre.org>
2017-03-23 16:38:09 +0000

    skeleton express app

---
+++ app.js
@@ -0,0 +1,41 @@
+'use strict';
+//THRID PARTY MODULES
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var app = express();
+
+var configuration = { rootpath: __dirname };
+require('./config/app')(app, configuration);
+
+
+app.use(logger('dev'));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ sample-config.js
@@ -0,0 +1,35 @@
+// THIS FILE SHOULD BE IN A CONFIG FOLDER ALONG WITH A CONFIG.JSON FILE
+// THIS IS JUST AN EXAMPLE FILE
+
+var express         = require('express');
+var path            = require('path');
+var favicon         = require('serve-favicon');
+var session         = require('express-session');
+var cookieParser    = require('cookie-parser');
+var bodyParser      = require('body-parser');
+var multer          = require('multer');
+var passport        = require('passport');
+
+
+module.exports = function(app, config) {
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+        next();
+    });
+    // view engine setup - USE JADE
+    app.set('views', path.join(config.rootPath, 'views'));
+    app.set('view engine', 'pug');
+    app.use(favicon(config.rootPath + '/public/favicon.ico')); //favicon
+    //use body parser for file uploads
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(multer({ dest: "../images/" }));
+
+    //COOKIES
+    app.use(cookieParser());
+
+    app.use(require('flash')());
+    app.use(express.static(path.join(config.rootPath, 'public')));
+}
